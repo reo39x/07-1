2d84edf8461dc830af173148885d99ac24fb45b7
reo39x <reo.39.xxx@gmail.com>
2022-06-21 00:53:06 +0900

    [add]記事詳細画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,6 +17,17 @@ class PostController extends Controller
     {
         return view('posts/index') -> with(['posts' => $post -> getPaginateBylimit()]);
     }
+    
+    /**
+     * 特定IDのpostを表示する
+     * 
+     * @params Object Post //引数の$postはid=1のPostインスタンス
+     * @return Response post view
+     */
+    public function show(Post $post)
+    {
+        return view('posts/show') -> with(['post' => $post]);
+    }
 }
 
 ?>
