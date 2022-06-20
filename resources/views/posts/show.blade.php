8e44e83409a6ee0bb07122d8d4813002fa04d487
reo39x <reo.39.xxx@gmail.com>
2022-06-21 01:00:46 +0900

    [add]記事詳細画面

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,17 +2,18 @@
 <html lang="{{ str_replace('_', '-', app() -> getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-        <h1>Blog Name</h1>
-            <div class='post'>
-                <h2 class='title'>{{ $post -> title }}</h2>
+        <h1 class='title'>{{ $post -> title }}</h1>
+            <div class='content'>
                 <p class='body'>{{ $post -> body }}</p>
                 <p class='updated_at'>{{ $post -> updated_at }}</p>
             </div>
-            <div class='back'>[<a href='/'>back</a>]</div>
+            <div class='footer'>[<a href='/'>back</a>]</div>
     </body>
 </html>
