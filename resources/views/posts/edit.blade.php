bc597bbf114f61d5d0ce1bb433da604f061d303b
reo39x <reo.39.xxx@gmail.com>
2022-06-24 01:16:20 +0900

    投稿編集画面の保存時入力チェック処理

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -12,10 +12,12 @@
                 <div class='content__title'>
                     <h2>タイトル</h2>
                     <input type='text' name='post[title]' value="{{ $post -> title }}">
+                    <p class="title__error" style="color:red">{{ $errors -> first('post.title') }}</p>
                 </div>
                 <div class='content__body'>
                     <h2>本文</h2>
                     <textarea name="post[body]">{{ $post -> body }}</textarea>
+                    <p class="body_error" style="color:red">{{ $errors -> first('post.body') }}</p>
                 </div>
                 <input type="submit" value="保存"/>
             </form>
