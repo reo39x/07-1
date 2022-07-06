c8b6934141f036a7ee111264e257277377a56bca
reo39x <reo.39.xxx@gmail.com>
2022-07-06 20:18:41 +0900

    リレーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use App\Http\Requests\PostRequest; //useする
+use App\Category;
 
 class PostController extends Controller
 {
@@ -29,9 +30,9 @@ class PostController extends Controller
         return view('posts/show') -> with(['post' => $post]);
     }
     
-    public function create()
+    public function create(Category $category)
     {
-        return view('posts/create');
+        return view('posts/create')->with(['categories' => $category->get()]);
     }
     
     public function store(Post $post, PostRequest $request) //引数をRequest->PostRequestにする
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,6 +22,7 @@
                 <p class='body'>{{ $post -> body }}</p>
                 <p class='updated_at'>{{ $post -> updated_at }}</p>
             </div>
+            <a href="/categories/{{ $post -> category -> id }}">{{ $post -> category -> name }}</a>
             <div class='footer'>[<a href='/'>back</a>]</div>
         
         <script>
